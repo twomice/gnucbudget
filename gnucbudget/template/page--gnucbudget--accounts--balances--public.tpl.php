5964e0399299f2780f7da1f870c7d07010b1e8d4
Allen Shaw <allen@JoineryHQ.com>
2017-02-04 16:52:19 -0600

    Added "rendered at [datetime]" in public balances footer.

--- gnucbudget/template/page--gnucbudget--accounts--balances--public.tpl.php
+++ gnucbudget/template/page--gnucbudget--accounts--balances--public.tpl.php
@@ -1,4 +1,5 @@
 <?php
 
 drupal_add_css(drupal_get_path('module', 'gnucbudget') . '/css/accounts-balances-public.css');
-print render($page['content']); 
+print render($page['content']);
+print ("Rendered at ". date('Y-m-d H:i:s'));
